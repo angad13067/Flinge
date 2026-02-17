0c0ca68c649fbd2f1941751bee8336dc3230931f
Angad <angad13067@gmail.com>
2026-02-16 20:16:14 -0500

    loading screen added

--- script.js
+++ script.js
@@ -29,4 +29,55 @@ function onScroll() {
 
 window.addEventListener("scroll", onScroll, { passive: true });
 
-///////////////////////////////////////////////
+//loader/////////////////////////////////////////////
+const loader = document.querySelector(".loader");
+
+function showLoader() {
+  loader.classList.remove("loader-hidden");
+}
+
+function hideLoader() {
+  loader.classList.add("loader-hidden");
+}
+
+//hide animation on first page load
+window.addEventListener("load", hideLoader);
+
+// Hide loader when the whole page (including images) is loaded
+window.addEventListener("load", () => {
+  hideLoader();
+});
+
+// Buttons/////////////////////////
+document.querySelector("#like-btn").addEventListener("click", async () => {
+  showLoader();
+
+  // do your real work here (changing card, fetching data, etc.)
+  await new Promise((resolve) => setTimeout(resolve, 1400));
+
+  hideLoader();
+});
+
+document.querySelector("#skip-btn").addEventListener("click", async () => {
+  showLoader();
+  await new Promise((resolve) => setTimeout(resolve, 1400));
+  hideLoader();
+});
+
+document.querySelector("#undo-btn").addEventListener("click", async () => {
+  showLoader();
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  hideLoader();
+});
+
+document.querySelectorAll(".footer-item").forEach((link) => {
+  link.addEventListener("click", async (e) => {
+    e.preventDefault();
+    // stop the link from navigating right away
+
+    showLoader();
+    await new Promise((resolve) => setTimeout(resolve, 1400));
+
+    window.location.href = link.href; // navigate after the delay
+  });
+});
